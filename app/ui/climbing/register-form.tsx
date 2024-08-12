def924935bb06534b5b6ab1592d5661812e82728
Paloma <paloma.aubeau@gmail.com>
2024-08-12 11:22:35 +0200

    Fix of register-form.tsx

--- app/ui/climbing/register-form.tsx
+++ app/ui/climbing/register-form.tsx
@@ -1,16 +1,26 @@
 'use client';
 
-import { useFormState } from 'react-dom';
-import { createClimbingMember } from '@/app/lib/actions/climbing/actions';
+import { useState } from 'react';
+import {
+  createClimbingMember,
+  ClimbingState,
+} from '@/app/lib/actions/climbing/actions';
 import FormRegistration from './FormRegistration';
 
 export default function RegisterForm() {
-  const initialState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(
-    (prevState: any, formData: any) =>
-      createClimbingMember(prevState, formData, true),
-    initialState,
-  );
+  const initialState: ClimbingState = {
+    isSuccess: undefined,
+    errors: {},
+    message: null,
+  };
+  const [state, setState] = useState<ClimbingState>(initialState);
+
+  const dispatch = async (formData: FormData): Promise<ClimbingState> => {
+    const newState = await createClimbingMember(state, formData, true);
+    setState(newState);
+    return newState;
+  };
+
   return (
     <div className="flex flex-col md:flex-row md:space-x-12 md:px-9">
       <div className="min-w-0 w-full md:w-1/2">
@@ -33,3 +43,36 @@ export default function RegisterForm() {
     </div>
   );
 }
+// Ancien code:
+// import { useFormState } from 'react-dom';
+// import { createClimbingMember } from '@/app/lib/actions/climbing/actions';
+// import FormRegistration from './FormRegistration';
+// export default function RegisterForm() {
+//   const initialState = { message: null, errors: {} };
+//   const [state, dispatch] = useFormState(
+//     (prevState: any, formData: any) =>
+//       createClimbingMember(prevState, formData, true),
+//     initialState,
+//   );
+//   return (
+//     <div className="flex flex-col md:flex-row md:space-x-12 md:px-9">
+//       <div className="min-w-0 w-full md:w-1/2">
+//         <h2 className="text-lg font-bold">LICENCE ET TARIFS</h2>
+//         <p className="my-2">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
+//           illum quae? Dolores, sapiente deserunt hic tenetur mollitia itaque
+//           placeat accusamus eum doloribus, in quaerat atque numquam. Hic numquam
+//           temporibus quos.
+//         </p>
+//         <h2 className="text-lg font-bold mt-8">HORAIRES</h2>
+//         <p className="mt-2 mb-8">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
+//           illum quae? Dolores, sapiente deserunt hic tenetur mollitia itaque
+//           placeat accusamus eum doloribus, in quaerat atque numquam. Hic numquam
+//           temporibus quos.
+//         </p>
+//       </div>
+//       <FormRegistration dispatch={dispatch} state={state} />
+//     </div>
+//   );
+// }
